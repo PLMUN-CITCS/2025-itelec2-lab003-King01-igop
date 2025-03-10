05fceaf6f519bf97dc8061321e650931cc8598ef
King Ocampo <ocampoking_bsit@plmun.edu.ph>
2025-03-10 13:05:18 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

--- python exercise_01.py
+++ python exercise_01.py
@@ -1,4 +1,4 @@
-# Your Name
+# HARI OCAMPOOOOOOOOOOOOOOOOO
 # ITELEC2
 # Laboratory #03 – Guided Coding Exercise:
 # Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
@@ -6,10 +6,15 @@
 count = 10                   # 'count' is assigned 10 (integer literal)
 Count = 15                   # 'Count' (different from 'count') is assigned 15
 decimal_value = 3.14         # 'decimal_value' is assigned 3.14 (float literal)
+
 message = "Hello, Python!"   # String literal
+
 is_active = True             # Boolean literal
+
 result = None                # None literal represents absence of value
+
 total_count = 20             # Another integer literal assignment
+
 print("Integer (count):", count)
 print("Integer (Count):", Count)
 print("Integer (total_count):", total_count)
